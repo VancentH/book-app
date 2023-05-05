ba891a41f3054fb3ce7acbfcf9eea415021a1980
hyde31456 <hyde31456@gmail.com>
2023-05-05 15:07:56 +0800

    <feat>: add update feature 1. enableUpdate: control display input, cancel and confirm buttons 2. idTobeUpdate: determine which item need to be work  in for loop

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,7 +9,21 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  books: Book[] = [];
+  books: Book[] = [
+    {
+      id: '1',
+      bookname: '1',
+      author: '1',
+    },
+    {
+      id: '2',
+      bookname: '2',
+      author: '2',
+    },
+  ];
+
+  enableUpdate: boolean = false;
+  idTobeUpdate: string = '';
   destroy$ = new Subject<void>();
 
   constructor(private bookService: BookService) {}
@@ -24,15 +38,59 @@ export class DashboardComponent implements OnInit {
   }
 
   getBooks(): void {
-    this.bookService.getAll().subscribe((books) => (this.books = books));
+    // this.bookService.getAll().subscribe((books) => (this.books = books));
   }
 
   deleteBook(book: Book): void {
+    // conform dialog
+    const result = confirm(`Remove item id: ${book.id} ?`);
+    if (result) {
+      this.bookService
+        .delete(book.id) // TODO: handle delete error
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(() => {
+          this.books = this.books.filter((item) => item !== book);
+          alert('Remove successfully');
+        });
+    }
+  }
+
+  updateBook(book: Book): void {
+    console.log('cancelUpdate');
+    this.enableUpdate = true;
+    this.idTobeUpdate = book.id;
+  }
+
+  cancelUpdate(): void {
+    console.log('cancelUpdate');
+    this.enableUpdate = false;
+    this.idTobeUpdate = '';
+
+  }
+
+  confirmUpdate(book: Book): void {
+    console.log('confirmUpdate');
+    console.log(book);
+
+    if (!book.bookname) {
+      alert('Book name must not be empty.');
+      return;
+    }
+    if (!book.author) {
+      alert('Author must not be empty.');
+      return;
+    }
+
+    console.log('successfully');
+    return;
     this.bookService
-      .delete(book.id)
+      .update(book)
       .pipe(takeUntil(this.destroy$))
       .subscribe(() => {
-        this.books = this.books.filter((item) => item !== book);
+        this.getBooks();
+        this.enableUpdate = false;
+        this.idTobeUpdate = '';
+        alert('Update successfully');
       });
   }
 
